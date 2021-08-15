b03ecf8f37b999c81248b753e5eb3ceee56a880e
BoikoVD <boiko.volodymyr.dm@gmail.com>
2021-08-15 21:22:12 +0300

    Version 1. Added 3 functions: getCMCResponse, sendAnswer, action. Added /startP and /stopP functional. Modified README.md in which describes the initial functionality

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # My CryptoPursuer telegram bot.
+
+## Version 1.
+
+#### Bot send every 60 sec messages with coins name which grew up on 5%.
+#### You can choose coins which want to pursuering, adding their symbol to array "pursueringCoins". Example:
+
+let pursueringCoins = [
+	"BTC",
+	"ETH",
+	"BNB",
+	"ICP",
+	"SOL",
+];
+
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const requestOptions = {
 	uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
 	qs: {
 		'start': '1',
-		'limit': '1',
+		'limit': '5000',
 		'convert': 'USD',
 		'percent_change_24h_min': '5',
 	},
@@ -20,15 +20,61 @@ const requestOptions = {
 	json: true,
 	gzip: true
 };
+//===========================================================
+let pursueringCoins = [
+	"BTC",
+	"ETH",
+	"BNB",
+	"ICP",
+	"SOL",
+];
 
+let users = [];
 
-rp(requestOptions).then(response => {
-	console.log('API call response:', response);
-}).catch((err) => {
-	console.log('API call error:', err.message);
+bot.onText(/\/startP/, (msg, match) => {
+	const chatId = msg.chat.id;
+	users.push(chatId);
+	console.log('user ' + chatId + ' started pursuering');
+	bot.sendMessage(chatId, 'Pursuering started');
+});
+bot.onText(/\/stopP/, (msg, match) => {
+	const chatId = msg.chat.id;
+	let userNum = users.indexOf(chatId);
+	users.splice(userNum, 1);
+	console.log('user ' + chatId + ' stopped pursuering');
+	bot.sendMessage(chatId, 'Pursuering stopped');
 });
 
+let responseCMC;
+setInterval(action, 60000);
 
 bot.on('message', (msg) => {
 	console.log('Message = ', msg.text);
 });
+//FUNCTIONS=============================================================
+async function action() {
+	await getCMCResponse();
+	sendAnswer(responseCMC, pursueringCoins);
+}
+
+async function getCMCResponse() {
+	await rp(requestOptions).then(response => {
+		responseCMC = response;
+	}).catch((err) => {
+		console.log('API call error:', err.message);
+	});
+}
+
+function sendAnswer(response, coinsArray, msgId) {
+	if (users.length > 0) {
+		for (let i = 0; i < Object.keys(response).length; i++) {
+			for (let j = 0; j < coinsArray.length; j++) {
+				if (response.data[i].symbol == coinsArray[j]) {
+					for (let k = 0; k < users.length; k++) {
+						bot.sendMessage(users[k], 'Coin ' + coinsArray[j] + ' up over than 5%')
+					}
+				}
+			}
+		}
+	}
+}
