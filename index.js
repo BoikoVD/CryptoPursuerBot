011e13677f18000f80a3cb9502f48a543a2dc643
BoikoVD <boiko.volodymyr.dm@gmail.com>
2021-08-29 06:26:33 +0300

    Version 2. Started work. Сhanged the structure of the bot. Created 4 new commands: /addC, /delC, /showC, /help

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { TOKENS } = require('./tokens.js');
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -10,9 +12,8 @@ const requestOptions = {
 	uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
 	qs: {
 		'start': '1',
-		'limit': '5000',
+		'limit': '2',
 		'convert': 'USD',
-		'percent_change_24h_min': '5',
 	},
 	headers: {
 		'X-CMC_PRO_API_KEY': TOKENS.CMC_TOKEN
@@ -22,6 +23,78 @@ const requestOptions = {
 };
 
 //===========================================================
+let userCoinLists = new Map();
+let userPercentValues = new Map();
+
+//COMMANDS
+bot.onText(/\/start/, (msg, match) => {
+	if (userCoinLists.has(msg.chat.id)) { } else {
+		userCoinLists.set(msg.chat.id, []);
+		userPercentValues.set(msg.chat.id, 5);
+		console.log('New user ' + msg.chat.id + ' connected');
+	}
+	bot.sendMessage(msg.chat.id, "Please use the /help command to learn more about my capabilities");
+});
+
+bot.onText(/\/addC (.+)/, (msg, match) => {
+	let coin = match[1].toUpperCase();
+	let coinList = userCoinLists.get(msg.chat.id);
+	if (coinList.includes(coin)) {
+		bot.sendMessage(msg.chat.id, "You have added this coin");
+	} else {
+		coinList.push(coin);
+		bot.sendMessage(msg.chat.id, coin + " added");
+	}
+});
+bot.onText(/\/delC (.+)/, (msg, match) => {
+	let coin = match[1].toUpperCase();
+	let coinList = userCoinLists.get(msg.chat.id);
+	if (coinList.includes(coin)) {
+		let index = coinList.indexOf(coin);
+		coinList.splice(index, 1);
+		bot.sendMessage(msg.chat.id, coin + " deleted");
+	} else {
+		bot.sendMessage(msg.chat.id, "You haven't added this coin");
+	}
+});
+bot.onText(/\/showC/, (msg, match) => {
+	let coinList = userCoinLists.get(msg.chat.id);
+	if (coinList.length == 0) {
+		bot.sendMessage(msg.chat.id, "You haven't added any coins");
+	} else {
+		bot.sendMessage(msg.chat.id, JSON.stringify(coinList, null, 2));
+	}
+});
+
+bot.onText(/\/help/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `Commands:
+	/addC - to add coin symbol to pursuing list. 
+	Example: addC btc
+	/delC - to delete coin symbol from pursuing list. 
+	Example: delC btc
+	/showC - to show all coins added to your pursuing list
+	/help - to show all commands
+	`);
+});
+//================
+
+bot.on('message', (msg) => {
+	console.log('Message = ', msg.text);
+});
+
+/*
+rp(requestOptions).then(response => {
+	for (let i = 0; i < Object.keys(response).length; i++) {
+		console.log(response.data[i].symbol);
+		console.log(response.data[i].quote.USD.percent_change_7d);
+	}
+}).catch((err) => {
+	console.log('API call error:', err.message);
+});
+*/
+
+//===========================================================
+/*
 let users = [];
 let pursueringCoins = [
 	'BTC',
@@ -44,7 +117,7 @@ bot.onText(/\/start/, (msg, match) => {
 bot.onText(/\/beginP/, (msg, match) => {
 	const chatId = msg.chat.id;
 	users.push(chatId);
-	console.log('user ' + chatId + ' started pursuering');
+	console.log('user ' + chatId + ' started pursuering' + match);
 	bot.sendMessage(chatId, 'Pursuering started');
 });
 bot.onText(/\/breakP/, (msg, match) => {
@@ -54,14 +127,14 @@ bot.onText(/\/breakP/, (msg, match) => {
 	console.log('user ' + chatId + ' stopped pursuering');
 	bot.sendMessage(chatId, 'Pursuering stopped');
 });
-/*
+
 bot.onText(/\/addC/, (msg, match) => {
-	
+
 });
 bot.onText(/\/delC/, (msg, match) => {
-	86400000
+
 });
-*/
+
 let responseCMC;
 setInterval(action, 60000);
 
@@ -106,3 +179,4 @@ function sendAnswer(response, coinsArray, msgId) {
 		}
 	}
 }
+*/
